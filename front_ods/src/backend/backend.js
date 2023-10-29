7d0dff7961871ab37e5d60303d4c868dbe12ffd9
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-10-29 12:41:08 -0500

    Backend Connection

---
+++ front_ods/src/backend/backend.js
@@ -0,0 +1,54 @@
+
+const BASE_URL = 'http://localhost:XXXX'
+
+export const getPredict = async function (data) {
+
+    const requestPredict = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+    
+    return fetch(BASE_URL + "/predict", requestPredict).then(async (response) => {
+        if (response.status === 412) {
+            throw new Error("Error: " + (await response.text()));
+        } else {
+            return response.json();
+        }
+    });
+
+}
+
+export const getHistory = async function (data) {
+
+    const requestGetHistory = {
+        method: "GET",
+    };
+    
+    return fetch(BASE_URL + "/history", requestGetHistory)
+        .then((response) => response.json())
+        .then((data) => data);
+    
+}
+
+export const postHistory = async function (data) {
+
+    const requestPostHistory = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    };
+    
+    return fetch(BASE_URL + "/history", requestPostHistory).then(async (response) => {
+        if (response.status === 412) {
+            throw new Error("Error: " + (await response.text()));
+        } else {
+            return response.json();
+        }
+    });
+
+}
